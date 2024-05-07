7449c957769a9218dfe59f65dcb2410d0f0fa78e
sanketdreja <sanketdreja@gmail.com>
2024-05-08 00:27:51 +0530

    Initial

--- src/WeatherCard.js
+++ src/WeatherCard.js
@@ -2,20 +2,20 @@ import React from "react";
 
 const WeatherCard = ({ data }) => {
   return (
-    <div className="weather-card">
-      <div>
+    <div>
+      <div className="weather-card">
         <p>Temperature: {data.temp_c}°C</p>
       </div>
-      <div>
+      <div className="weather-card">
         {" "}
         <p>Humidity: {data.humidity}%</p>
       </div>
 
-      <div>
+      <div className="weather-card">
         {" "}
         <p>Condition: {data.condition.text}</p>
       </div>
-      <div>
+      <div className="weather-card">
         {" "}
         <p>Wind Speed: {data.wind_kph} km/h</p>
       </div>
